9b61d0a5fc6b18e2690e4f1801c71dc24f49a448
wangddwei <1482466683@qq.com>
2023-03-10 22:20:20 +0800

    do while 和 死循环

---
+++ com/itwang/loop/DieLoop.java
@@ -0,0 +1,22 @@
+package com.itwang.loop;
+
+/**
+ * 死循环
+ */
+public class DieLoop {
+    public static void main(String[] args) {
+/*        //1.for
+        for(;;){
+            System.out.println("123");
+        }*/
+/*        //2.while
+        while (true){
+            System.out.println("123");
+        }*/
+        //3.do while
+        do {
+            System.out.println("123");
+        }while (true);
+
+    }
+}
---
+++ com/itwang/loop/DoWhileDemo.java
@@ -0,0 +1,10 @@
+package com.itwang.loop;
+public class DoWhileDemo {
+    public static void main(String[] args) {
+        int i = 0;
+        do{
+            System.out.println("123");
+            i++;
+        }while(i<3);
+    }
+}
