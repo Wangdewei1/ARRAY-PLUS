eb736d51c2517e8e3ac8d1d225409f8a5fc97617
wangddwei <1482466683@qq.com>
2023-03-10 21:13:10 +0800

    求奇数和   方法二

--- com/itwang/loop/ForDemo4.java
+++ com/itwang/loop/ForDemo4.java
@@ -5,6 +5,7 @@ package com.itwang.loop;
  */
 public class ForDemo4 {
     public static void main(String[] args) {
+        //1.方法一
         //2.定义sum
         int sum = 0;
         //1.定义每个奇数数据
@@ -14,5 +15,13 @@ public class ForDemo4 {
         }
         System.out.println(sum);
 
+        //2.方法二
+        int sum1 = 0;
+        for (int i = 1; i <=100 ; i++) {
+            if (i % 2 != 0){
+                sum1 += i;
+            }
+        }
+        System.out.println(sum1);
     }
 }
