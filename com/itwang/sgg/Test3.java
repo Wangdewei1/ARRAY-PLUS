f87e5da68cc0a63c3f384e43dc7e74e048c64db2
wangddwei <1482466683@qq.com>
2023-03-27 01:00:03 +0800

    练习简单问题 判断奇偶

---
+++ com/itwang/sgg/Test3.java
@@ -0,0 +1,36 @@
+package com.itwang.sgg;
+
+import java.util.Scanner;
+
+/**
+ * 3.判断一个整数是奇数还是偶数
+ */
+public class Test3 {
+    public static void main(String[] args) {
+        //3.
+        Scanner sc = new Scanner(System.in);
+        loop: while (true) {
+            System.out.println("-------------判断一个整数是奇数还是偶数--------------");
+            System.out.println("1.请输入要判断的数字：");
+            System.out.println("2.退出判断");
+            int number = sc.nextInt();
+            switch (number){
+                case 1 ->
+                        System.out.println(isIntNumber(number));
+                case 2 -> {
+                    System.out.println("已退出");
+                    break loop;
+                }
+                default ->
+                        System.out.println("系统错误");
+            }
+        }
+    }
+    public static String isIntNumber(int number){
+        if (number % 2==0){
+            return number + "：为偶数";
+        }else {
+            return number + "：为奇数";
+        }
+    }
+}
